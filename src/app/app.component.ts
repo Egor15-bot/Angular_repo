de3679f8ec31a1a7b09fce13ddc86e4ad7ff050e
BondarEgorTest <erop_96_15111@mail.ru>
2024-03-31 11:22:38 +1000

    Lesson 12 added sendForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,15 +14,15 @@ import { RouterModule } from '@angular/router';
     RouterModule
   ],
   template: `
-  <main>
-  <a [routerLink]="['/']">
-  <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-  </header>
-  </a>
-  <section class="content">
-  <router-outlet></router-outlet>
-  </section>
+ <main>
+    <a [routerLink]="['/']">
+    <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    </a>
+    <section class="content">
+    <router-outlet></router-outlet>
+    </section>
 </main>
 `,
   styleUrls: ['./app.component.css']
